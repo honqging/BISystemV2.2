5fe76e2f6930bb839b15a1e080825f45552278ca
Qing Hong <hongqingnothing@163.com>
2017-06-25 19:32:29 +0800

    add page: 按台时常统计图+按台均时常统计图

---
+++ js/antaijunshichangEcharts.js
@@ -0,0 +1,166 @@
+var ATJSCpage = 1,
+	doc = document,
+	ATJSCurlStartTime = "2010-01-01",
+	ATJSCurlEndTime = currentDate,
+	// ATJSCEchartsUrl = "",
+	ATJSCstartDate = doc.getElementById("ATJSCstartTime"),
+	ATJSCendDate = doc.getElementById("ATJSCendTime"),
+	ATJSCsubmitDate = doc.getElementById("ATJSCsubmitTime");
+	ATJSCexport = doc.getElementById("ATJSCexport");
+
+var ATJSCpageBefore = doc.getElementById("ATJSCPageBefore"),
+	ATJSCpageNext = doc.getElementById("ATJSCPageNext"),
+	ATJSCpageNum = doc.getElementById("ATJSCPageNum");
+
+ATJSCpageBefore.onclick = function(){
+	if(ATJSCpage==1){alert("已经是第一页");}
+	else{
+		ATJSCpage --;
+		ATJSCcharts();
+	}
+}
+
+ATJSCpageNext.onclick = function(){
+	console.log("1"+ATJSCpage);
+	ATJSCpage ++;
+	console.log("2"+ATJSCpage);
+	ATJSCcharts();
+}
+
+//设定时间
+ATJSCsubmitDate.onclick = function () {
+	getDate(ATJSCstartDate,ATJSCendDate);
+	ATJSCurlStartTime = getDate(ATJSCstartDate,ATJSCendDate)[0],
+		ATJSCurlEndTime = getDate(ATJSCstartDate,ATJSCendDate)[1];
+	ATJSCcharts();
+}
+
+ATJSCexport.onclick = function () {
+	// no interface yet
+}
+addLoadEvent(initialPicker(ATJSCstartDate, ATJSCendDate));
+
+function ATJSCcharts(){
+	require.config({
+		paths: {
+			echarts: './build/dist' //引用资源文件夹路径，注意路径
+		}
+	});
+	require(
+		[
+			'echarts',
+			'echarts/chart/bar'     //柱形图
+			//'echarts/chart/line'    //折线图
+		],
+		function (ec) {
+			var ATJSCEchartsUrl = "http://123.206.134.34:8080/Medicals_war/charts/antaijunshichang?page="+ATJSCpage+"&startTime="+ATJSCurlStartTime+"&endTime="+ATJSCurlEndTime;
+			var url = ATJSCEchartsUrl;
+			console.log(url);
+			var dataSource = [];
+			$.ajax({
+				type: "get",
+				url: url,
+				dataType: "json",
+				jsonp:"callback",
+				success: function (data) {
+					dataSource = data;
+					ATJSCpageNum.placeholder = ATJSCpage;
+					//console.log(dataSource);
+					addData();
+				},
+				error: function (XMLHttpRequest, textStatus, errorThrown) {
+					alert(errorThrown);
+				}
+			});
+			//result ajax
+			function addData(){
+				var myChart = ec.init(document.getElementById('ATJSC_Echarts'));
+				var ecConfig = require('echarts/config');
+				var colors = ['#FC4779'];
+				var option = {
+					color: colors,
+					title: {
+						text: '手术间',
+						textStyle : {
+							fontSize : '15',
+							fontWeight : 'normal'
+						},
+						x: 'center',
+						y: 'bottom'
+					},
+					tooltip: {
+						trigger: 'axis',
+						axisPointer: {
+							type: 'cross'
+						}
+					},
+					grid: {
+						right: '20%',
+						y2: 60
+					},
+					toolbox: {
+						feature: {
+							dataView: {show: true, readOnly: false},
+							restore: {show: true},
+							saveAsImage: {show: true}
+						}
+					},
+					//legend: {
+					//	data: ['科室名称'],
+					//	align: 'left'
+					//},
+					xAxis: [
+						{
+							type: 'category',
+						   /*  axisTick: {
+								alignWithLabel: true
+							}, */
+							data: dataSource.x,
+							axisLabel: {
+								//横轴信息全部显示
+								interval:0,
+
+								//横轴信息文字每行显示三个
+								formatter:function(val){
+									var newVal = '';
+									var vList = val.split("");
+									for(var j = 0; j<vList.length; j++){
+										if(j%12 == 0 && j!=0){
+											newVal += '\n';
+										}
+										newVal += vList[j];
+									}
+									return newVal;
+								}
+							}
+						}
+					],
+					yAxis: [
+						{
+							type: 'value',
+							name: '分钟',
+							min: 0
+						}
+					],
+					series: [
+						{
+							name:'分钟',
+							type:'bar',
+							barCategoryGap: '70%',
+							itemStyle: {
+								normal: {
+									label : {
+										show : true
+									}
+								}
+							},
+							data:dataSource.y
+						}
+					]
+				};
+				myChart.setOption(option);
+			}
+		}
+	);
+}
+addLoadEvent(ATJSCcharts());
---
+++ js/antaishichangEcharts.js
@@ -0,0 +1,166 @@
+var ATSCpage = 1,
+	doc = document,
+	ATSCurlStartTime = "2010-01-01",
+	ATSCurlEndTime = currentDate,
+	// ATSCEchartsUrl = "",
+	ATSCstartDate = doc.getElementById("ATSCstartTime"),
+	ATSCendDate = doc.getElementById("ATSCendTime"),
+	ATSCsubmitDate = doc.getElementById("ATSCsubmitTime");
+	ATSCexport = doc.getElementById("ATSCexport");
+
+var ATSCpageBefore = doc.getElementById("ATSCPageBefore"),
+	ATSCpageNext = doc.getElementById("ATSCPageNext"),
+	ATSCpageNum = doc.getElementById("ATSCPageNum");
+
+ATSCpageBefore.onclick = function(){
+	if(ATSCpage==1){alert("已经是第一页");}
+	else{
+		ATSCpage --;
+		ATSCcharts();
+	}
+}
+
+ATSCpageNext.onclick = function(){
+	console.log("1"+ATSCpage);
+	ATSCpage ++;
+	console.log("2"+ATSCpage);
+	ATSCcharts();
+}
+
+//设定时间
+ATSCsubmitDate.onclick = function () {
+	getDate(ATSCstartDate,ATSCendDate);
+	ATSCurlStartTime = getDate(ATSCstartDate,ATSCendDate)[0],
+		ATSCurlEndTime = getDate(ATSCstartDate,ATSCendDate)[1];
+	ATSCcharts();
+}
+
+ATSCexport.onclick = function () {
+	// no interface yet
+}
+addLoadEvent(initialPicker(ATSCstartDate, ATSCendDate));
+
+function ATSCcharts(){
+	require.config({
+		paths: {
+			echarts: './build/dist' //引用资源文件夹路径，注意路径
+		}
+	});
+	require(
+		[
+			'echarts',
+			'echarts/chart/bar'     //柱形图
+			//'echarts/chart/line'    //折线图
+		],
+		function (ec) {
+			var ATSCEchartsUrl = "http://123.206.134.34:8080/Medicals_war/charts/antaishichang?page="+ATSCpage+"&startTime="+ATSCurlStartTime+"&endTime="+ATSCurlEndTime;
+			var url = ATSCEchartsUrl;
+			console.log(url);
+			var dataSource = [];
+			$.ajax({
+				type: "get",
+				url: url,
+				dataType: "json",
+				jsonp:"callback",
+				success: function (data) {
+					dataSource = data;
+					ATSCpageNum.placeholder = ATSCpage;
+					//console.log(dataSource);
+					addData();
+				},
+				error: function (XMLHttpRequest, textStatus, errorThrown) {
+					alert(errorThrown);
+				}
+			});
+			//result ajax
+			function addData(){
+				var myChart = ec.init(document.getElementById('ATSC_Echarts'));
+				var ecConfig = require('echarts/config');
+				var colors = ['#F944C6'];
+				var option = {
+					color: colors,
+					title: {
+						text: '手术间',
+						textStyle : {
+							fontSize : '15',
+							fontWeight : 'normal'
+						},
+						x: 'center',
+						y: 'bottom'
+					},
+					tooltip: {
+						trigger: 'axis',
+						axisPointer: {
+							type: 'cross'
+						}
+					},
+					grid: {
+						right: '20%',
+						y2: 60
+					},
+					toolbox: {
+						feature: {
+							dataView: {show: true, readOnly: false},
+							restore: {show: true},
+							saveAsImage: {show: true}
+						}
+					},
+					//legend: {
+					//	data: ['科室名称'],
+					//	align: 'left'
+					//},
+					xAxis: [
+						{
+							type: 'category',
+						   /*  axisTick: {
+								alignWithLabel: true
+							}, */
+							data: dataSource.x,
+							axisLabel: {
+								//横轴信息全部显示
+								interval:0,
+
+								//横轴信息文字每行显示三个
+								formatter:function(val){
+									var newVal = '';
+									var vList = val.split("");
+									for(var j = 0; j<vList.length; j++){
+										if(j%12 == 0 && j!=0){
+											newVal += '\n';
+										}
+										newVal += vList[j];
+									}
+									return newVal;
+								}
+							}
+						}
+					],
+					yAxis: [
+						{
+							type: 'value',
+							name: '分钟',
+							min: 0
+						}
+					],
+					series: [
+						{
+							name:'分钟',
+							type:'bar',
+							barCategoryGap: '70%',
+							itemStyle: {
+								normal: {
+									label : {
+										show : true
+									}
+								}
+							},
+							data:dataSource.y
+						}
+					]
+				};
+				myChart.setOption(option);
+			}
+		}
+	);
+}
+addLoadEvent(ATSCcharts());
