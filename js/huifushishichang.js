f13711b9d4d002db85911a6ed2f7c2194c8fddfd
Qing Hong <hongqingnothing@163.com>
2017-07-16 12:11:43 +0800

    fix bug: add select page & num per page for 恢复室时长

--- js/huifushishichang.js
+++ js/huifushishichang.js
@@ -1,10 +1,17 @@
+var HFSSCTotal = doc.getElementById("HFSSCTotal"),
+	HFSSCTotalPage = 0,
+	HFSSCnumPerPage = doc.getElementById("HFSSCnumPerPage"),
+	HFSSCnumPer = 20,
+	HFSSCassignPage = doc.getElementById("HFSSCassignPage"),
+	HFSSCconfirm = doc.getElementById("HFSSCconfirm");
+
 var HFSSCloadpage = 1,
 	HFSSCdataSource = [],
 	HFSSCdataTitle = [],
 	doc = document,
 	HFSSCurlStartTime = "2010-01-01",
 	HFSSCurlEndTime = currentDate,
-	HFSSCurl = "http://123.206.134.34:8080/Medicals_war/recovery/morethan1hour?page="+HFSSCloadpage+"&startTime="+HFSSCurlStartTime+"&endTime="+HFSSCurlEndTime,
+	HFSSCurl = "http://123.206.134.34:8080/Medicals_war/recovery/morethan1hour?rowCount="+ HFSSCnumPer +"&page="+HFSSCloadpage+"&startTime="+HFSSCurlStartTime+"&endTime="+HFSSCurlEndTime,
 	HFSSCstartDate = doc.getElementById("HFSSCstartTime"),
 	HFSSCendDate = doc.getElementById("HFSSCendTime"),
 	HFSSCsubmitDate = doc.getElementById("HFSSCsubmitTime");
@@ -18,6 +25,8 @@ $.ajax({
           success: function (data) {
 			  HFSSCdataSource = data.data;
 			  HFSSCdataTitle = data.header;
+			  HFSSCTotalPage = data.pageCount;
+
 			  insertHFSSCTable();
 		  },
 		  error: function (XMLHttpRequest, textStatus, errorThrown) {
@@ -120,6 +129,8 @@ function insertHFSSCTable(){
 		}
 		table.appendChild(tr);
 	}
+
+	HFSSCTotal.innerHTML = HFSSCTotalPage;
 }
 
 //��ҳ
@@ -132,7 +143,7 @@ var HFSSCbeforePage = doc.getElementById("HFSSCPageBefore"),
 		else{
 			HFSSCloadpage --;
 			//console.log(HFSSCloadpage);
-			var url2 = "http://123.206.134.34:8080/Medicals_war/recovery/morethan1hour?page="+HFSSCloadpage+"&startTime="+HFSSCurlStartTime+"&endTime="+HFSSCurlEndTime;
+			var url2 = "http://123.206.134.34:8080/Medicals_war/recovery/morethan1hour?rowCount="+ HFSSCnumPer +"&page="+HFSSCloadpage+"&startTime="+HFSSCurlStartTime+"&endTime="+HFSSCurlEndTime;
 			$.ajax({
 				  type: "get",
 				  url: url2,
@@ -141,6 +152,8 @@ var HFSSCbeforePage = doc.getElementById("HFSSCPageBefore"),
 				  success: function (data) {
 					  HFSSCdataSource = data.data;
 					  HFSSCdataTitle = data.header;
+					  HFSSCTotalPage = data.pageCount;
+
 					  HFSSCPageNum.placeholder = HFSSCloadpage;
 					  insertHFSSCTable();
 					   },
@@ -152,30 +165,36 @@ var HFSSCbeforePage = doc.getElementById("HFSSCPageBefore"),
 	}
 HFSSCnextPage.onclick = function(){
 	HFSSCloadpage ++;
-	var url2 = "http://123.206.134.34:8080/Medicals_war/recovery/morethan1hour?page="+HFSSCloadpage+"&startTime="+HFSSCurlStartTime+"&endTime="+HFSSCurlEndTime;
-	$.ajax({
-		  type: "get",
-		  url: url2,
-		  dataType: "json",
-		  jsonp:"callback",
-		  success: function (data) {
-			  HFSSCdataSource = data.data;
-			  HFSSCdataTitle = data.header;
-			  HFSSCPageNum.placeholder = HFSSCloadpage;
-			  insertHFSSCTable();
-			   },
-		  error: function (XMLHttpRequest, textStatus, errorThrown) {
-		  alert(errorThrown);
-		 }
-	 });
+	var url2 = "http://123.206.134.34:8080/Medicals_war/recovery/morethan1hour?rowCount="+ HFSSCnumPer +"&page="+HFSSCloadpage+"&startTime="+HFSSCurlStartTime+"&endTime="+HFSSCurlEndTime;
+	if(HFSSCloadpage > HFSSCTotalPage){
+		alert('已经是最后一页');
+	}else {
+		$.ajax({
+			type: "get",
+			url: url2,
+			dataType: "json",
+			jsonp: "callback",
+			success: function (data) {
+				HFSSCdataSource = data.data;
+				HFSSCdataTitle = data.header;
+				HFSSCTotalPage = data.pageCount;
+
+				HFSSCPageNum.placeholder = HFSSCloadpage;
+				insertHFSSCTable();
+			},
+			error: function (XMLHttpRequest, textStatus, errorThrown) {
+				alert(errorThrown);
+			}
+		});
 	}
+}
 
 // 按日期搜索
 HFSSCsubmitDate.onclick = function () {
     getDate(HFSSCstartDate,HFSSCendDate);
 	HFSSCurlStartTime = getDate(HFSSCstartDate,HFSSCendDate)[0],
 	HFSSCurlEndTime = getDate(HFSSCstartDate,HFSSCendDate)[1];
-	var urlTime = "http://123.206.134.34:8080/Medicals_war/recovery/morethan1hour?page="+HFSSCloadpage+"&startTime="+HFSSCurlStartTime+"&endTime="+HFSSCurlEndTime;
+	var urlTime = "http://123.206.134.34:8080/Medicals_war/recovery/morethan1hour?rowCount="+ HFSSCnumPer +"&page="+HFSSCloadpage+"&startTime="+HFSSCurlStartTime+"&endTime="+HFSSCurlEndTime;
     $.ajax({
         type: "get",
         url: urlTime,
@@ -184,13 +203,90 @@ HFSSCsubmitDate.onclick = function () {
         success: function (data) {
 			HFSSCdataSource = data.data;
 			HFSSCdataTitle = data.header;
-            //console.log(SMdataSource);
+			HFSSCTotalPage = data.pageCount;
+
+			//console.log(HFSSCdataSource);
             insertHFSSCTable();
         },
         error: function (XMLHttpRequest, textStatus, errorThrown) {
             alert(errorThrown);
         }
     });
+}
+
+function isInteger(obj) {
+	return typeof obj === 'number' && obj%1 === 0 && obj > 0
+}
+
+HFSSCconfirm.onclick = function(){
+	tempPage = HFSSCloadpage;
+	HFSSCloadpage = parseFloat(HFSSCassignPage.value);
+	if(isInteger(HFSSCloadpage)){
+		console.log(HFSSCloadpage);
+		if(HFSSCloadpage <= HFSSCTotalPage){
+			var url2 = "http://123.206.134.34:8080/Medicals_war/recovery/morethan1hour?rowCount="+ HFSSCnumPer +"&page="+HFSSCloadpage+"&startTime="+HFSSCurlStartTime+"&endTime="+HFSSCurlEndTime;
+			console.log(url2);
+			$.ajax({
+				type: "get",
+				url: url2,
+				dataType: "json",
+				jsonp:"callback",
+				success: function (data) {
+					HFSSCdataSource = data.data;
+					HFSSCdataTitle = data.header;
+					HFSSCTotalPage = data.pageCount;
+					HFSSCPageNum.placeholder = HFSSCloadpage;
+					insertHFSSCTable();
+				},
+				error: function (XMLHttpRequest, textStatus, errorThrown) {
+					alert(errorThrown);
+				}
+			});
+		}else{
+			HFSSCloadpage = tempPage;
+			alert('超出页数上限，请重新选择页数');
+		}
+	}else{
+		alert('请输入正整数！')
+	}
+}
+
+HFSSCnumPerPage.onchange = function(){
+	var tempPer = HFSSCnumPer,
+		tempTotalPage = HFSSCTotalPage,
+		tempSelected = HFSSCnumPer;
+	var p1 = $(this).children('option:selected').val();//这就是selected的值
+	HFSSCnumPer = p1;
+	var url2 = "http://123.206.134.34:8080/Medicals_war/recovery/morethan1hour?rowCount="+ HFSSCnumPer +"&page="+HFSSCloadpage+"&startTime="+HFSSCurlStartTime+"&endTime="+HFSSCurlEndTime;
+	$.ajax({
+		type: "get",
+		url: url2,
+		dataType: "json",
+		jsonp:"callback",
+		success: function (data) {
+			HFSSCdataSource = data.data;
+			HFSSCdataTitle = data.header;
+			HFSSCTotalPage = data.pageCount;
+
+			if(HFSSCTotalPage < HFSSCloadpage){
+				alert('超出数据量上限，请重新选择页数或者每页数据条数');
+				HFSSCnumPer = tempPer;
+				HFSSCTotalPage = tempTotalPage;
+				for(var i = 0; i < HFSSCnumPerPage.options.length; i++){
+					if(HFSSCnumPerPage.options[i].innerHTML == tempSelected){
+						HFSSCnumPerPage.options[i].selected = true;
+						break;
+					}
+				}
+			}else{
+				insertHFSSCTable();
+			}
+			console.log(url2);
+		},
+		error: function (XMLHttpRequest, textStatus, errorThrown) {
+			alert(errorThrown);
+		}
+	});
 }
 
 HFSSCexport.onclick = function () {
