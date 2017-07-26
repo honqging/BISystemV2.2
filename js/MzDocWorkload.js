77e3351831f1700dec4f7d9908595e99e2f0a38a
Qing Hong <hongqingnothing@163.com>
2017-07-26 16:32:06 +0800

    new feature: multi-line table, top feature of 麻醉医生工作量

--- js/MzDocWorkload.js
+++ js/MzDocWorkload.js
@@ -51,30 +51,112 @@ function insertMZYSGZLTable(){
     thead.innerHTML = '';
 
     //单独添加表头
+    var top = doc.getElementById('MZYSGZL_table_top');
+    if(MzDocWorkloadpage != 1){
+        top.style.display = 'none';
+    }else{
+        top.style.display = 'block';
+    }
+    var th = doc.createElement('th'),
+        span = doc.createElement('span');
+    span.innerHTML = '🔝';
+    th.appendChild(span);
+    th.style.width = '2%';
+    th.style.padding = '8px';
+    thead.appendChild(th);
+
     var th = doc.createElement("th"),
         thData = doc.createTextNode('科室'),
         td = doc.createElement("td");
     th.appendChild(thData);
-    th.style.width = '20%';
+    th.style.width = '19%';
     thead.appendChild(th);
     for(var t=0; t<MZYSGZLtableTiTle.length; t++){
         var th = doc.createElement("th"),
             thData = doc.createTextNode(MZYSGZLtableTiTle[t]);
         th.appendChild(thData);
-        th.style.width = '20%';
+        th.style.width = '19%';
         thead.appendChild(th);
     }
 
+    var colorLen = new Array(MZYSGZLtableData.length);
+    var colorLenD = new Array(MZYSGZLtableData.length);
     for(var x=0; x<MZYSGZLtableData.length; x++){
         for(var i=0; i<MZYSGZLtableData[x].groupRows.length; i++){
             var tr = doc.createElement("tr");
+
+            var td = doc.createElement('td'),
+                span = doc.createElement('span');
+            span.innerHTML = '🔝';
+            td.appendChild(span);
+            td.style.width = '2%';
+            if(x == 0){
+                console.log('x i: ', x, i);
+                colorLen[x] = '#F9F9F9';
+                colorLenD[x] = '#F5F5F5';
+            }else{
+                if(MZYSGZLtableData[x-1].groupRows.length % 2 == 0){
+                    colorLen[x] = colorLen[x-1];
+                    colorLenD[x] = colorLenD[x-1];
+                }else{
+                    colorLen[x] = colorLenD[x-1];
+                    colorLenD[x] = colorLen[x-1];
+                }
+            }
+            td.style.backgroundColor = colorLen[x];
+
+            if(i==0){
+                td.style.borderTop = '1px #D6D6D6 solid';
+                console.log('bColor: ', td.style.backgroundColor);
+            }else{
+                td.style.border = '0px';
+            }
+
+            tr.appendChild(td);
+            tr.onclick = function(){
+                $(this).find('span').css('visibility', 'visible');
+            };
+            var trLen = MZYSGZLtableData[x].groupRows[0].length;
+            console.log('trLen', trLen);
+            td.trlen = trLen + 1;
+            var param = { trLen: td.trlen };
+            $(td).click(param, function(event){
+                var trLen = event.data.trLen;
+                if($(this).find('span').css('background-color') != 'rgb(255, 255, 0)'){
+                    var trr = $(this).parent().clone(true);
+                    console.log('start', trr.children('td').length, trLen, 'end');
+
+                    trr.children().first().css('border-top', '1px #D6D6D6 solid');
+                    //trr.children().first().next('border-right', '0px');
+                    if(trr.children('td').length == trLen){
+                        var addData = doc.createTextNode(trr.attr('title'));
+                        var addTd = doc.createElement('td');
+                        addTd.appendChild(addData);
+                        addTd.style.borderRight = '0px';
+                        trr.children().first().after(addTd);
+                    }else{
+                        trr.children().first().next().attr('rowspan', 1);
+                        trr.children().first().next().css('border-right', '0px');
+
+                    }
+                    console.log(trr.children('td').length);
+                    $('#MZYSGZL_table_top').prepend(trr);
+                    $(this).find('span').css('background-color', 'yellow');
+                    $(this).find('span').css('visibility', 'hidden');
+
+                    alert('成功置顶');
+                }else{
+                    alert('该项已置顶');
+                }
+            });
+
             for(var j=-1; j<MZYSGZLtableData[x].groupRows[i].length; j++){
                 if(j==-1 && i==0){
                     var data = doc.createTextNode(MZYSGZLtableData[x].groupName),
                         td = doc.createElement("td");
                     td.title = "office";
                     td.appendChild(data);
-                    td.style.width = '20%';
+                    td.style.width = '19%';
                     tr.appendChild(td);
                 }
                 else{
@@ -94,7 +176,11 @@ function insertMZYSGZLTable(){
                             html:true,
                             content:'<div id="content">loading...</div>'
                         });
-						a.onclick = function(){
+
+                        var param = { department: a.department, name: a.name };
+                        $(a).click(param, function(event){
+                            var department = event.data.department,
+                                name = event.data.name;
                             var result;
                             $("[data-toggle='popover']").popover({
                                 html:true,
@@ -102,25 +188,25 @@ function insertMZYSGZLTable(){
                             });
                             $.ajax({
                                   type: "get",
-                                  url: "http://123.206.134.34:8080/Medicals_war/statistic/mazuiyishengQuery?name="+this.name+"&department="+this.department+"&startTime="+MZYSGZLurlStartTime+"&endTime="+MZYSGZLurlEndTime,
+                                  url: "http://123.206.134.34:8080/Medicals_war/statistic/mazuiyishengQuery?name="+name+"&department="+department+"&startTime="+MZYSGZLurlStartTime+"&endTime="+MZYSGZLurlEndTime,
                                   dataType: "json",
                                   jsonp:"callback",
                                   success: function (data) {
-                                                  result = data;
-                                                  var wholeDiv = doc.createElement("div");
-                                                  for(var i=0;i<result.length;i++){
-                                                      var eachData = doc.createTextNode(result[i]);
-                                                      var p = doc.createElement("p");
-                                                      p.appendChild(eachData);
-                                                      wholeDiv.appendChild(p);
-                                                  }
-                                                  $('#content').html(wholeDiv);
-                                                   },
+                                      result = data;
+                                      var wholeDiv = doc.createElement("div");
+                                      for(var i=0;i<result.length;i++){
+                                          var eachData = doc.createTextNode(result[i]);
+                                          var p = doc.createElement("p");
+                                          p.appendChild(eachData);
+                                          wholeDiv.appendChild(p);
+                                      }
+                                      $('#content').html(wholeDiv);
+                                       },
                                   error: function (XMLHttpRequest, textStatus, errorThrown) {
                                   alert(errorThrown);
                                  }
                              });
-                        }
+                        });
 						a.appendChild(data);
 						td.appendChild(a);
 					}
@@ -130,9 +216,10 @@ function insertMZYSGZLTable(){
 						td.title = MZYSGZLtableData[x].groupRows[i][j];
 						td.appendChild(data);
 					}
-                    td.style.width = '20%';
+                    td.style.width = '19%';
                     //td.style.textAlign = 'left';
                     tr.appendChild(td);
+                    tr.title = MZYSGZLtableData[x].groupName;
                 }
             }
             table.appendChild(tr);
@@ -144,11 +231,11 @@ function insertMZYSGZLTable(){
 		//w设为公有变量，否则每次会对表格再次重头进行遍历。
        for(var w=0;w<totalRow;w++,del++) {
            if (del !== titleRow) {
-               table.rows[del].deleteCell(0);
-               table.rows[titleRow].cells[0].rowSpan = totalRow;
+               table.rows[del].deleteCell(1);
+               table.rows[titleRow].cells[1].rowSpan = totalRow;
            }
-           table.rows[titleRow].cells[0].style.verticalAlign = 'middle';
-           table.rows[titleRow].cells[0].style.borderRight = '1px #D6D6D6 solid';
+           table.rows[titleRow].cells[1].style.verticalAlign = 'middle';
+           table.rows[titleRow].cells[1].style.borderRight = '1px #D6D6D6 solid';
        }
 	   titleRow += totalRow;
     }
@@ -222,6 +309,7 @@ MZYSGZLsubmitDate.onclick = function () {
     MZYSGZLurlStartTime = getDate(MZYSGZLstartDate,MZYSGZLendDate)[0],
     MZYSGZLurlEndTime = getDate(MZYSGZLstartDate,MZYSGZLendDate)[1];
     MzDocWorkloadpage = 1;
+    doc.getElementById('SSHD_table_top').innerHTML = '';
     var urlTime = "http://123.206.134.34:8080/Medicals_war/statistic/mazuiyisheng?rowCount="+ MZYSGZLnumPer +"&page="+MzDocWorkloadpage+"&startTime="+MZYSGZLurlStartTime+"&endTime="+MZYSGZLurlEndTime;
     $.ajax({
         type: "get",
